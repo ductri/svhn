68112d023f4ef72a8a192ffc2b6db91d900ddcbf
ductri <ductricse@gmail.com>
2018-05-29 11:05:34 +0700

    ok

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-nvidia-docker run -v `pwd`:/source/ \
-    -v /root/code/all_dataset:/all_dataset
+nvidia-docker run --rm -v `pwd`:/source/ \
+    -v /root/code/all_dataset:/all_dataset \
     --name trind_svhn trind/svhn \
-    /bin/bash -c "python train.py --ALL_DATASET_PATH=/all_dataset"
+    /bin/bash -c "python train.py --ALL_DATASET_PATH=/all_dataset" \
+    -- TEST_SIZE = 5000
